a11a7a0ff058a4c767cf22de65ed4c7a56cbf7af
jais210 <jackelineisabel.puruaya@gmail.com>
2017-10-02 08:14:18 -0500

    js

--- player-scoreboard/app.jsx
+++ player-scoreboard/app.jsx
@@ -1,29 +1,116 @@
-let PLAYERS = [
-  {
-    name: "Jim Hoskins",
-    score: 31,
-    id: 1,
-  },
-   {
-    name: "Andree Hoskins",
-    score: 35,
-    id: 2,
-  },
-   {
-    name: "Alena Hoskins",
-    score: 42,
-    id: 3,
-  },
+let players = [
+  // arreglo de objetos
+	{
+		name: "Jim Hoskins",
+		score: 31,
+		id: 1,
+	},
+	{
+		name: "Andree Hoskins",
+		score: 35,
+		id: 2,
+	},
+	{
+		name: "Alena Hoskins",
+		score: 42,
+		id: 3,
+	},
 ];
 
-
-
-const Application = ({title, players}) => {
-   return (
-      <Header players={players}/>
-      <PlayerList players={players}/>
-      <PlayerForm />      
-   ) ;
+function score (){
+  // recorro el arreglo para sacar de cada objeto solo su score
+	return players.map((player) =>{
+		return (player.score);
+	});
 }
 
-ReactDOM.render(<Application title="Scoreboard" players = {PLAYERS}/>, document.getElementById('container'));
+function addScore (){
+  // recorro cada objeto para coger su score y devolver la suma total
+	let scores = score(players);
+	return scores.reduce((back,now) =>{
+		return back+ now;
+	},0);
+}
+
+function showPlayers (){
+  return players.map((player, index)=>{
+	return (
+		<div className="player">
+			<div className="player-name">
+				<center><strong>{player.name}</strong></center> 
+			</div>
+			<div className="player-score counter">
+				<div className="counter-action decrement">
+					-
+				</div>
+				<div className="counter-score">
+					{player.score}
+				</div>
+				<div className=" counter-action increment">
+					+
+				</div>
+			</div>
+		</div>
+	);
+  });
+}
+
+const Header = (props) => {
+  	return(
+		<div className="header">
+			<div className="stats">
+				<table>
+					<tr>
+						<td>Player: </td>
+						<td className="letter"><strong>{props.players.length}</strong></td>
+					</tr>
+					<tr>
+						<td>Total Points: </td>
+						<td className="letter"><strong>{addScore(props.players)}</strong></td>
+					</tr>
+				</table>
+			</div>
+			<h1><strong>{props.title}</strong></h1>
+			<div className="stopwatch">
+				<h2>STOPWATCH</h2>
+				<div className="stopwatch-time">
+					0
+				</div>
+				<div>
+					<button><strong>START</strong></button>
+					<button><strong>RESET</strong></button>
+				</div>
+			</div>
+		</div>
+  	);
+} 
+
+const PlayerList = (props) => {
+  return (
+	  <div>{showPlayers(props.players)}</div> 
+  );
+}
+
+const PlayerForm = (props) => {
+  return (
+	<div className="add-player-form">
+		<form>
+			<input type="text"placeholder="ENTER A NAME"/>
+			<input type="submit" value="ADD PLAYER"/>
+		</form>
+	</div>
+  );
+}
+
+// creo la etiqueta
+const TableroScore = ({title, players}) => {
+   return (
+	 <div className="scoreboard">
+		<Header players={players} title={title}/>
+		<PlayerList players={players}/>
+		<PlayerForm />
+	  </div>      
+   );
+}
+
+ReactDOM.render(<TableroScore title="Scoreboard" players = {players}/>, document.getElementById('container'));
